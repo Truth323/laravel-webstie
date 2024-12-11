903ca3de7033e5405caca5f1b1beb969763f298b
Truth323 <okuzaki323@gmail.com>
2024-12-11 21:36:41 +0900

    laravel website first commit

---
+++ resources/js/Shared/DefaultLayout.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="bg-gray-100 h-screen">
+        <slot/>
+    </div>
+</template>
+
+<script>
+export default {
+name: "DefaultLayout"
+}
+</script>
+
+<style scoped>
+
+</style>
